6588c37c90d25f012d7fd88e46d18d3b4bbc11e5
Erybor <vmama20@freeuni.edu.ge>
2022-02-12 04:12:27 +0400

    Did only the regex bit for now, work in progress, unsure about the interface of Lexer class / whether to return a token or a list of tokens.

--- main.py
+++ main.py
@@ -1,7 +1,67 @@
-# This is a sample Python script.
+import re
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# class TokenType(Enum):
+#     NUMERIC_LITERAL,
+#     REGISTER,
+#     PC_REG,
+#     SP_REG,
+#     RV_REG,
+#     MEM,
+#     LB,
+#     RB,
+#     OP,
+#     COMMA,
+#     BRANCH_OP,
+#     JUMP_OP,
+#     CALL_OP,
+#     LT_OP,
+#     GT_OP,
+#     EQ_OP,
+#     IDENTIFIER
+
+class Token(object):
+    type: str
+    value: int
+
+
+class Lexer(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        pass
+
+    def get_next_token(self):
+        token_specification = [
+            ('NUMERIC_LITERAL', '\d+'),  # Integer or decimal number
+            ('REGISTER', 'R\d+'),  #
+            ('PC_REG', '^PC$'),  #
+            ('SP_REG', '^SP$'),  #
+            ('RV_REG', '^RV$'),  #
+            ('MEM', '^MEM$'),  #
+            ('LB', '\['),  #
+            ('RB', '\]'),  #
+            ('COMMA', ','),
+            ('ARITHM_OP', r'[-+*\]'),
+            ('BRANCH_OP', 'B((LT)|(LE)|(GT)|(GE)|(EQ)|(NE))'), # matches branch operators: BLT, BLE, BGT, BGE, BEQ, BNE
+            ('JUMP_OP', '^JMP$'),
+            ('CALL_OP', '^CALL$'),
+            ('LT', '<'),
+            ('GT', '>'),
+            ('EQ', '='),
+            ('IDENTIFIER', '[a-zA-Z]+'),
+            ('OTHER', '.') # everything else
+        ]
+        tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+
+
+
+    pass
+
+
+
+
+
+
 
 
 def print_hi(name):
@@ -13,4 +73,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
