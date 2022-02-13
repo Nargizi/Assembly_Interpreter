dee5e5dd9cb42128e6cfd431c645c44e1ff58ca5
Erybor <vmama20@freeuni.edu.ge>
2022-02-14 02:50:14 +0400

    (Hopefully) working implementation of a Tokenizer. Currently, it only accepts string, so we need to change that to generator. Unit tests needed.

--- main.py
+++ main.py
@@ -1,75 +1,78 @@
 import re
+from typing import NamedTuple
 
-# class TokenType(Enum):
-#     NUMERIC_LITERAL,
-#     REGISTER,
-#     PC_REG,
-#     SP_REG,
-#     RV_REG,
-#     MEM,
-#     LB,
-#     RB,
-#     OP,
-#     COMMA,
-#     BRANCH_OP,
-#     JUMP_OP,
-#     CALL_OP,
-#     LT_OP,
-#     GT_OP,
-#     EQ_OP,
-#     IDENTIFIER
 
-class Token(object):
+class Token(NamedTuple):  # not sure what this does tbh
+    def __init(self, type, value):
+        self.type = type
+        self.value = value
+
     type: str
-    value: int
+    value: str
+    # line: int
+    # column: int
 
-
-class Lexer(object):
+class Tokenizer:
     def __init__(self, text):
-        self.text = text
+        self.code = text
         self.pos = 0
-        pass
+        # self.current_char = self.text[self.pos]
 
     def get_next_token(self):
+
         token_specification = [
+            ('REGISTER', 'R\d+(?!\w)'),
             ('NUMERIC_LITERAL', '\d+'),  # Integer or decimal number
-            ('REGISTER', 'R\d+'),  #
-            ('PC_REG', '^PC$'),  #
-            ('SP_REG', '^SP$'),  #
-            ('RV_REG', '^RV$'),  #
-            ('MEM', '^MEM$'),  #
+            ('PC_REG', '^PC(?!\w)'),  #
+            ('SP_REG', '^SP(?!\w)'),  #
+            ('RV_REG', '^RV(?!\w)'),  #
+            ('MEM', 'M(?=\[)'),  #
             ('LB', '\['),  #
             ('RB', '\]'),  #
             ('COMMA', ','),
-            ('ARITHM_OP', r'[-+*\]'),
-            ('BRANCH_OP', 'B((LT)|(LE)|(GT)|(GE)|(EQ)|(NE))'), # matches branch operators: BLT, BLE, BGT, BGE, BEQ, BNE
-            ('JUMP_OP', '^JMP$'),
-            ('CALL_OP', '^CALL$'),
+            ('INT_CAST', '\.\d'),  # change the name to something better, matches .1, .2 ...
+            ('ARITHM_OP', '[\-\+\*\/]'),
+            ('BRANCH_OP', 'B(LT|LE|GT|GE|EQ|NE)'),  # matches branch operators: BLT, BLE, BGT, BGE, BEQ, BNE
+            ('JUMP_OP', '^JMP\b'),  # not sure if this is correct
+            ('CALL_OP', '^CALL\b'),
             ('LT', '<'),
             ('GT', '>'),
             ('EQ', '='),
+            ('NEWLINE', '\n'),
+            ('IGNORED', '\s+'),
             ('IDENTIFIER', '[a-zA-Z]+'),
-            ('OTHER', '.') # everything else
+            ('OTHER', '.')  # everything else
         ]
-        tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+        if self.pos > len(self.code) - 1:
+            self.pos += 1
+            return Token('EOF', None)
+
+        for token_type, token_regex in token_specification:
+            match = re.match(token_regex, self.code[self.pos:])
+            if not match:
+                continue
+            if token_type == 'IGNORED':
+                self.pos += match.end()
+                return self.get_next_token()
+            if token_type == 'OTHER':
+                raise RuntimeError("Error: unexpected symbol: '" + match.group() + "' on line {self.pos}")
+            self.pos += match.end()
+            return Token(token_type, match.group())
+
+# test = '''R123 = 12
+#         Mem[R2] = 12
+#         R3 = R2
+#     ENDIF;
+# '''
 
 
+test = "M[R1 + 4] =.1 R2"
 
-    pass
-
-
-
-
-
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    tokenizer = Tokenizer(test)
+    while True:
+        token = tokenizer.get_next_token()
+        if token.type == "EOF":
+            break
+        else:
+            print("shemovida",  token.type, token.value)
